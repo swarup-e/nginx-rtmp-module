764ce13aacfde554e83940428995355a062d1ce1
swarup-e <swarup@datakaveri.org>
2021-02-01 23:03:02 +0530

    Added change for recording start check

--- ngx_rtmp_record_module.c
+++ ngx_rtmp_record_module.c
@@ -1109,26 +1109,27 @@ ngx_rtmp_record_node_avd(ngx_rtmp_session_t *s, ngx_rtmp_record_rec_ctx_t *rctx,
     {
         return NGX_OK;
     }
+    if (brkframe && rctx->started) {
+        if (rracf->interval != NGX_CONF_UNSET_MSEC)
+        {
+        // record interval should work if set, manual mode or not
+            next = rctx->last;
+            next.msec += rracf->interval;
+            next.sec  += (next.msec / 1000);
+            next.msec %= 1000;
 
-    if (rracf->interval != NGX_CONF_UNSET_MSEC)
-    {
-	// record interval should work if set, manual mode or not
-        next = rctx->last;
-        next.msec += rracf->interval;
-        next.sec  += (next.msec / 1000);
-        next.msec %= 1000;
-
-        if (ngx_cached_time->sec  > next.sec ||
-           (ngx_cached_time->sec == next.sec &&
-           ngx_cached_time->msec > next.msec))
-            {
-		ngx_rtmp_record_node_close(s, rctx);
-                ngx_rtmp_record_node_open(s, rctx);
-            }
-    }
-    else if (!rctx->failed  && !(rracf->flags & NGX_RTMP_RECORD_MANUAL))
-    {
-        ngx_rtmp_record_node_open(s, rctx);
+            if (ngx_cached_time->sec  > next.sec ||
+            (ngx_cached_time->sec == next.sec &&
+            ngx_cached_time->msec > next.msec))
+                {
+                    ngx_rtmp_record_node_close(s, rctx);
+                    ngx_rtmp_record_node_open(s, rctx);
+                }
+        }
+        else if (!rctx->failed  && !(rracf->flags & NGX_RTMP_RECORD_MANUAL))
+        {
+            ngx_rtmp_record_node_open(s, rctx);
+        }
     }
 
     if (!rctx->initialized) {
